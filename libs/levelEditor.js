281c00a2d0820b2cee00cdbdc3d05ccb3294c4f3
Gareth <gareth.marland@gmail.com>
2016-10-28 01:08:32 -0400

    tweaking

--- libs/levelEditor.js
+++ libs/levelEditor.js
@@ -136,14 +136,14 @@
 			var geometry = new THREE.Geometry();
 
 			for ( var i = - size; i <= size; i += step ) {
-				geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-				geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+				geometry.vertices.push(new THREE.Vector3(-size, 0, i));
+				geometry.vertices.push(new THREE.Vector3(size, 0, i));
 
-				geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-				geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+				geometry.vertices.push(new THREE.Vector3(i, 0, - size));
+				geometry.vertices.push(new THREE.Vector3(i, 0,   size));
 			}
 
-			var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+			var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.15, transparent: true } );
 
 			return new THREE.LineSegments( geometry, material );
         }
